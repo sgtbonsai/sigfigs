c9e26671168293f8fae9a6f5882d7e92a41d9594
Leo Tran <racingbyleo@gmail.com>
2023-06-19 12:02:36 -0500

    added support for decimals

--- sigfigs.js
+++ sigfigs.js
@@ -8,30 +8,27 @@ const rl = readline.createInterface({
 
 // count the number of sig figs
 function countSignificantFigures(number) {
-  const numString = String(number).replace(/[^0-9.]/g, ''); // remove things that aren't numbers
+  const numString = String(number).replace(/[^0-9.eE+-]/g, ''); // remove things that aren't numbers
   const decimalIndex = numString.indexOf('.');
-  
+
   let significantFigures = 0;
   let countZeros = false;
 
   for (let i = 0; i < numString.length; i++) {
     const char = numString[i];
-    
-    if (char === '.' || char === 'e') {
+
+    if (char === '.' || char === 'e' || char === 'E' || char === '+' || char === '-') {
       continue;
     }
-    
+
     if (char === '0') {
-      if (significantFigures > 0) {
+      if (significantFigures > 0 || countZeros || i > decimalIndex) {
         countZeros = true;
+        significantFigures++;
       }
     } else {
       significantFigures++;
       countZeros = false;
-    }
-    
-    if (countZeros && decimalIndex !== -1 && i > decimalIndex) {
-      break;
     }
   }
 
@@ -42,20 +39,20 @@ function countSignificantFigures(number) {
 function startProgram() {
   rl.question('> Enter a number: ', (input) => {
     const number = Number(input);
-    
+
     if (isNaN(number)) {
       console.log('Please input a valid number.');
       restartProgram();
     } else {
       const result = countSignificantFigures(number);
       console.log(`> Number of significant figures: ${result}`);
-      console.log("> Developed by BonsaiSgt on June 19, 2023");
+      console.log("> Developed by Leo Tran on June 19, 2023");
       rl.close();
     }
   });
 }
 
-// restart
+// restart of NaN detected
 function restartProgram() {
   startProgram();
 }
